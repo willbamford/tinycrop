d3b3a402ce509188fa4ef484bdfd7aa4e533fe01
Will Bamford <w.bamford@gmail.com>
2015-11-22 10:31:06 +0000

    Add build steps and skeleton directory structure

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+var ImageCrop = React.createClass({
+  render: function() {
+    return (
+      <div className="ImageCrop">
+        Hello, world! I am the "ImageCrop" component
+      </div>
+    );
+  }
+});
+
+module.exports = ImageCrop;
