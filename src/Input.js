4c7879d7daa2641ff51beae4e847486042a10d84
Will Bamford <w.bamford@gmail.com>
2015-11-27 07:52:08 +0000

    Created a simple class to abstract over single touch/mouse pointer events.

---
+++ src/Input.js
@@ -0,0 +1,67 @@
+var Listeners = require('./Listeners.js');
+
+var Input = function(domElement) {
+
+	this.domElement = domElement;
+	this.listeners = Listeners.create();
+
+	function createEventForMouse(source) {
+		return {
+			source: source,
+			x: source.offsetX,
+			y: source.offsetY,
+			type: 'Mouse'
+		};
+	}
+
+	function createEventForTouch(source) {
+		var bounds = source.target.getBoundingClientRect();
+		var touch = source.touches.length > 0 ? source.touches[0] : source.changedTouches[0];
+		return {
+			source: source,
+			x: touch ? touch.pageX - bounds.left,
+			y: touch ? touch.pageY - bounds.top,
+			type: 'Touch'
+		};
+	}
+
+	domElement.addEventListener('mousedown', function(source) {
+		this.listeners.notify('down', createEventForMouse(source));
+	}.bind(this));
+
+	domElement.addEventListener('touchstart', function(source) {
+		this.listeners.notify('down', createEventForTouch(source));
+	}.bind(this));
+
+	domElement.addEventListener('mousemove', function(source) {
+		this.listeners.notify('move', createEventForMouse(source));
+	}.bind(this));
+
+	domElement.addEventListener('touchmove', function(source) {
+		this.listeners.notify('move', createEventForTouch(source));
+	}.bind(this));
+
+	domElement.addEventListener('mouseup', function(source) {
+		this.listeners.notify('up', createEventForMouse(source));
+	}.bind(this));
+
+	domElement.addEventListener('touchend', function(source) {
+		this.listeners.notify('up', createEventForTouch(source));
+	}.bind(this));
+};
+
+Input.create = function(domElement) {
+	return new Input(domElement);
+};
+
+Input.prototype.on = function(type, fn) {
+	this.listeners.on(type, fn);
+	return this;
+};
+
+Input.prototype.off = function(type, fn) {
+	this.listeners.off(type, fn);
+	return this;
+};
+
+module.exports = Input;
--- src/SelectionLayer.js
+++ src/SelectionLayer.js
@@ -1,3 +1,5 @@
+var Input = require('./Input.js');
+
 var SelectionLayer = function(opts) {
 
 	this.bounds = {
@@ -17,6 +19,20 @@ var SelectionLayer = function(opts) {
 	this.canvas = opts.canvas;
 	this.context = this.canvas.getContext('2d');
 	this.target = opts.target;
+
+	this.input = Input.create(this.canvas);
+
+	this.input.on('down', function(e) {
+		e.source.preventDefault();
+	});
+
+	this.input.on('move', function(e) {
+		e.source.preventDefault();
+	});
+
+	this.input.on('up', function(e) {
+		e.source.preventDefault();
+	});
 };
 
 SelectionLayer.create = function(opts) {
@@ -31,20 +47,26 @@ SelectionLayer.prototype.revalidate = function() {
 
 	if (target.image) {
 
-		bounds.x = target.bounds.x + target.bounds.width * (region.x / target.image.width);
-		bounds.y = target.bounds.y + target.bounds.height * (region.y / target.image.height);
-		bounds.width = target.bounds.width * (region.width / target.image.width);
-		bounds.height = target.bounds.height * (region.height / target.image.height);
+		bounds.x = Math.round(target.bounds.x + target.bounds.width * (region.x / target.image.width));
+		bounds.y = Math.round(target.bounds.y + target.bounds.height * (region.y / target.image.height));
+		bounds.width = Math.round(target.bounds.width * (region.width / target.image.width));
+		bounds.height = Math.round(target.bounds.height * (region.height / target.image.height));
 	}
 };
 
 SelectionLayer.prototype.paint = function() {
 
+	var canvas = this.canvas;
 	var bounds = this.bounds;
 
 	var context = this.context;
-	context.fillStyle = 'rgba(255, 0, 0, 0.5)';
-	context.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
+
+	context.fillStyle = 'rgba(0, 0, 0, 0.5)';
+
+	context.fillRect(0, 0, canvas.width, bounds.y);
+	context.fillRect(0, bounds.y, bounds.x, bounds.height);
+	context.fillRect(bounds.x + bounds.width, bounds.y, canvas.width - bounds.x + bounds.width, bounds.height);
+	context.fillRect(0, bounds.y + bounds.height, canvas.width, canvas.height - bounds.y + bounds.height);
 };
 
 module.exports = SelectionLayer;
